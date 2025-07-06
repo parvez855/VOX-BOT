78669d2d20efa3ecdb9b75bc6d83216463a271da
parvez855 <130054027+parvez855@users.noreply.github.com>
2025-07-06 18:03:29 +0600

    speak.js

--- speak.js
+++ speak.js
@@ -1,35 +1,45 @@
 const { createAudioResource, createAudioPlayer, joinVoiceChannel, AudioPlayerStatus } = require('@discordjs/voice');
+const googleTTS = require('google-tts-api');
 const fs = require('fs');
 const path = require('path');
-const gTTS = require('node-gtts'); // ✅ correct package
+const https = require('https');
 
 module.exports = async function speak(text, voiceChannel, lang = 'bn') {
+  const url = googleTTS.getAudioUrl(text, {
+    lang: lang,
+    slow: false,
+    host: 'https://translate.google.com',
+  });
+
   const filename = path.join(__dirname, 'voice.mp3');
-  const tts = gTTS(lang);
 
   return new Promise((resolve, reject) => {
-    const writeStream = fs.createWriteStream(filename);
-    tts.stream(text)
-      .pipe(writeStream)
-      .on('finish', () => {
-        const connection = joinVoiceChannel({
-          channelId: voiceChannel.id,
-          guildId: voiceChannel.guild.id,
-          adapterCreator: voiceChannel.guild.voiceAdapterCreator
+    const file = fs.createWriteStream(filename);
+    https.get(url, (response) => {
+      response.pipe(file);
+      file.on('finish', () => {
+        file.close(() => {
+          const connection = joinVoiceChannel({
+            channelId: voiceChannel.id,
+            guildId: voiceChannel.guild.id,
+            adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+          });
+
+          const resource = createAudioResource(filename);
+          const player = createAudioPlayer();
+
+          player.play(resource);
+          connection.subscribe(player);
+
+          player.on(AudioPlayerStatus.Idle, () => {
+            fs.unlinkSync(filename);
+            connection.destroy();
+            resolve();
+          });
         });
-
-        const resource = createAudioResource(filename);
-        const player = createAudioPlayer();
-
-        player.play(resource);
-        connection.subscribe(player);
-
-        player.on(AudioPlayerStatus.Idle, () => {
-          fs.unlinkSync(filename);
-          connection.destroy();
-          resolve();
-        });
-      })
-      .on('error', reject);
+      });
+    }).on('error', (err) => {
+      fs.unlink(filename, () => reject(err));
+    });
   });
 };
