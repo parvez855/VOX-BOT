31b0423ed9cb56055d32b4e86ffcbd7cccb8ba23
parvez855 <130054027+parvez855@users.noreply.github.com>
2025-07-07 02:14:47 +0600

    User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  userId: String,
-  joinPhrase: String,
-  leavePhrase: String,
-  language: { type: String, default: 'bn' }
+  userId: { type: String, required: true, unique: true },
+  language: { type: String, default: 'en' },
+  joinPhrase: { type: String, default: null },
+  leavePhrase: { type: String, default: null }
 });
 
 module.exports = mongoose.model('User', userSchema);
