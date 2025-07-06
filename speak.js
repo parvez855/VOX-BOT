340aabf2b69286398fe8615f5492d185ff2b1614
parvez855 <130054027+parvez855@users.noreply.github.com>
2025-07-06 23:35:00 +0600

    speak.js

--- speak.js
+++ speak.js
@@ -1,45 +1,38 @@
-const { createAudioResource, createAudioPlayer, joinVoiceChannel, AudioPlayerStatus } = require('@discordjs/voice');
+const { createAudioPlayer, createAudioResource, joinVoiceChannel, AudioPlayerStatus } = require('@discordjs/voice');
 const googleTTS = require('google-tts-api');
-const fs = require('fs');
-const path = require('path');
-const https = require('https');
+const playdl = require('play-dl');
 
 module.exports = async function speak(text, voiceChannel, lang = 'bn') {
-  const url = googleTTS.getAudioUrl(text, {
-    lang: lang,
-    slow: false,
-    host: 'https://translate.google.com',
-  });
-
-  const filename = path.join(__dirname, 'voice.mp3');
-
-  return new Promise((resolve, reject) => {
-    const file = fs.createWriteStream(filename);
-    https.get(url, (response) => {
-      response.pipe(file);
-      file.on('finish', () => {
-        file.close(() => {
-          const connection = joinVoiceChannel({
-            channelId: voiceChannel.id,
-            guildId: voiceChannel.guild.id,
-            adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-          });
-
-          const resource = createAudioResource(filename);
-          const player = createAudioPlayer();
-
-          player.play(resource);
-          connection.subscribe(player);
-
-          player.on(AudioPlayerStatus.Idle, () => {
-            fs.unlinkSync(filename);
-            connection.destroy();
-            resolve();
-          });
-        });
-      });
-    }).on('error', (err) => {
-      fs.unlink(filename, () => reject(err));
+  try {
+    const url = googleTTS.getAudioUrl(text, {
+      lang,
+      slow: false,
+      host: 'https://translate.google.com',
     });
-  });
+
+    const stream = await playdl.stream(url);
+
+    const connection = joinVoiceChannel({
+      channelId: voiceChannel.id,
+      guildId: voiceChannel.guild.id,
+      adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+    });
+
+    const player = createAudioPlayer();
+    const resource = createAudioResource(stream.stream, {
+      inputType: stream.type,
+    });
+
+    player.play(resource);
+    connection.subscribe(player);
+
+    return new Promise((resolve) => {
+      player.on(AudioPlayerStatus.Idle, () => {
+        connection.destroy();
+        resolve();
+      });
+    });
+  } catch (err) {
+    console.error("TTS Error:", err);
+  }
 };
